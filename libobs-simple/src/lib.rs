baeefeb7953cb80005b91c46d904fb4051254b5e
Hendrik Lind <hendrik@sshcrack.me>
2025-11-28 21:15:46 +0100

    chore: add docs flag for os specific sources

--- libobs-simple/src/lib.rs
+++ libobs-simple/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "__test_environment", feature(doc_cfg))]
 //! A simplified interface for recording and streaming with libobs
 
 pub mod output;
--- libobs-simple/src/sources/mod.rs
+++ libobs-simple/src/sources/mod.rs
@@ -1,3 +1,4 @@
+//!
 //! A library for creating OBS sources without having to figure out what properties are used by sources.
 //!
 //! This crate provides convenient builders for OBS sources across different platforms:
@@ -6,10 +7,12 @@
 //!
 //! You can find examples [here](https://github.com/libobs-rs/libobs-rs/tree/main/examples).
 
-#[cfg(target_family = "windows")]
+#[cfg(any(windows, doc))]
+#[doc(cfg(windows))]
 pub mod windows;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", doc))]
+#[doc(cfg(target_os = "linux"))]
 pub mod linux;
 
 mod macro_helper;
